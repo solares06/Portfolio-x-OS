2794284ce263c51473a17b6f87feb5dd0a250dbb
Souranil Sen <souracode@gmail.com>
2026-07-13 02:32:06 +0530

    Configure Next.js to ignore lint and type errors during build

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,12 @@ const withPWA = withPWAInit({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
